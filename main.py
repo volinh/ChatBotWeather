c0804431ed25c94702ca709013556582c3a3d716
volinh <volinh275@gmail.com>
2018-05-02 04:07:59 +0700

    them vai tag

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from app.chatbot import Chatbot
 from adapter.ner import train
-# train.train()
-# train.main()
-chatbot = Chatbot()
-chatbot.main()
+train.train()
+train.main()
+# chatbot = Chatbot()
+# chatbot.main()
 #
 # import re
 #
